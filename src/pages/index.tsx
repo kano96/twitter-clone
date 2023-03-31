170c211c996d7e535cdde6e7522d3a19dfcd9c3b
Kevin Torres <kevin.torres@devsu.com>
2023-03-31 15:48:29 -0500

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 
+import { api } from "~/utils/api";
+
 const Home: NextPage = () => {
   const user = useUser();
+  const { data } = api.post.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -16,7 +20,11 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
