4b02921e081e7b63a5888c0d2196e0a4e92139f6
Kevin Torres <kevin.torres@devsu.com>
2023-04-03 16:14:08 -0500

    working posts

--- src/components/CreatePostWizard.tsx
+++ src/components/CreatePostWizard.tsx
@@ -1,7 +1,17 @@
 import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
+import { useState } from "react";
+import { api } from "~/utils/api";
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [input, setInput] = useState("");
+  const ctx = api.useContext();
+  const { mutate, isLoading: isPosting } = api.post.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.post.getAll.invalidate();
+    },
+  });
 
   if (!user) {
     return null;
@@ -19,7 +29,11 @@ const CreatePostWizard = () => {
         type="text"
         placeholder="What is happening?"
         className="grow bg-transparent outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
