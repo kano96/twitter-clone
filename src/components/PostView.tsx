d5f27db211e55eb7c6af512c4edfbd317f38736b
Kevin Torres <kevin.torres@devsu.com>
2023-04-05 08:47:32 -0500

    New files for new routes

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -2,6 +2,7 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -21,8 +22,12 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-xl">{post.content}</span>
       </div>
